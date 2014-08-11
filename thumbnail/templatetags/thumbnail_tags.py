580fb52339e70672b2a27d6ee4f7f1bdbcf91773
intellisense <eye-see-you@live.com>
2014-08-12 03:02:16 +0500

    Adding support for model level THUMBNAIL_OPTIONS_DICT

--- thumbnail/templatetags/thumbnail_tags.py
+++ thumbnail/templatetags/thumbnail_tags.py
@@ -16,26 +16,33 @@ class CustomThumbnailNode(ThumbnailNode):
     """ Extends ThumbnailNode to use thumbnail sizes from settings"""
     error_message = ('Please enter sizes defined in settings')
     def __init__(self, parser, token):
+        self.parser = parser
+        self.token = token
         ThumbnailNode.__init__(self, parser, token)
-        bits = token.split_contents()
+    
+    def render_(self, context):
+        file_ = self.file_.resolve(context)
+        bits = self.token.split_contents()
         if len(bits) < 5 or bits[-2] != 'as':
             raise TemplateSyntaxError(self.error_msg)
         try:
-            thumbnail_settings = get_thumbnail_options_from_dict(settings.OPTIONS_DICT)
+            thumbnail_settings = get_thumbnail_options_from_dict(
+                getattr(file_.instance, 'THUMBNAIL_OPTIONS_DICT', settings.OPTIONS_DICT))
             thumbnail_options = thumbnail_settings[bits[2]]
             thumbnail_options = thumbnail_options.split()
         except KeyError:
             raise TemplateSyntaxError(self.error_message)
-        self.geometry = parser.compile_filter("'%s'" % thumbnail_options[0])
+        self.geometry = self.parser.compile_filter("'%s'" % thumbnail_options[0])
         self.options = []
         try:
             m = kw_pat.match(thumbnail_options[1])
             key = smart_str(m.group('key'))
-            expr = parser.compile_filter('"%s"' % m.group('value'))
+            expr = self.parser.compile_filter('"%s"' % m.group('value'))
             self.options.append((key, expr))
         except IndexError:
             pass
         self.as_var = bits[-1]
+        return ThumbnailNode.render_(self, context)
 
 @register.tag
 def thumbnail(parser, token):
