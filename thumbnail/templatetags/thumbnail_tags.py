34f1d568fd9d74182e73d49d6453145372d3a556
intellisense <eye-see-you@live.com>
2015-04-19 08:00:16 +0500

    bug fixes, context was not available in init

--- thumbnail/templatetags/thumbnail_tags.py
+++ thumbnail/templatetags/thumbnail_tags.py
@@ -16,9 +16,13 @@ class CustomThumbnailNode(ThumbnailNode):
     """ Extends ThumbnailNode to use thumbnail sizes from settings"""
     error_message = ('Please enter sizes defined in settings')
     def __init__(self, parser, token):
+        self.parser = parser
+        self.token = token
         ThumbnailNode.__init__(self, parser, token)
+    
+    def pre_render(self, context):
         file_ = self.file_.resolve(context)
-        bits = token.split_contents()
+        bits = self.token.split_contents()
         if len(bits) < 5 or bits[-2] != 'as':
             raise TemplateSyntaxError(self.error_msg)
         try:
@@ -28,16 +32,20 @@ class CustomThumbnailNode(ThumbnailNode):
             thumbnail_options = thumbnail_options.split()
         except KeyError:
             raise TemplateSyntaxError(self.error_message)
-        self.geometry = parser.compile_filter("'%s'" % thumbnail_options[0])
+        self.geometry = self.parser.compile_filter("'%s'" % thumbnail_options[0])
         self.options = []
         try:
             m = kw_pat.match(thumbnail_options[1])
             key = smart_str(m.group('key'))
-            expr = parser.compile_filter('"%s"' % m.group('value'))
+            expr = self.parser.compile_filter('"%s"' % m.group('value'))
             self.options.append((key, expr))
         except IndexError:
             pass
         self.as_var = bits[-1]
+    
+    def _render(self, context):
+        self.pre_render(context)
+        return ThumbnailNode._render(self, context)
 
 @register.tag
 def thumbnail(parser, token):
